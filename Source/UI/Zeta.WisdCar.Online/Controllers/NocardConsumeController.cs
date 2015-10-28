11e007836ee274d45f57f53a9b77d8b0acc62695
fudongdong <fuxi1988@163.com>
2015-10-28 15:44:47 +0800

    非会员现金消费完结

--- Source/UI/Zeta.WisdCar.Online/Controllers/NocardConsumeController.cs
+++ Source/UI/Zeta.WisdCar.Online/Controllers/NocardConsumeController.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Mvc;
 using Zeta.WisdCar.Business.CustClubCardModule;
+using Zeta.WisdCar.Business.RechargeConsumeModule;
 using Zeta.WisdCar.Infrastructure;
+using Zeta.WisdCar.Infrastructure.Helper;
 using Zeta.WisdCar.Infrastructure.Log;
+using Zeta.WisdCar.Model.VO;
 using Zeta.WisdCar.Online.App_Start;
 using Zeta.WisdCar.Online.Models;
 
@@ -58,6 +62,86 @@ namespace Zeta.WisdCar.Online.Controllers
             }
 
             return Json(data, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult SubNocardConsume()
+        {
+            ReturnedData data = new ReturnedData();
+            try
+            {
+                int payType = NullHelper.Convert<int>(Request["payType"], -1);
+                int custID = NullHelper.Convert<int>(Request["custID"], -1);
+                string consItem = NullHelper.Convert<string>(Request["data"], "");
+                var comlist= JsonSerializer(consItem);
+                var cust = new CustomerMgm().GetCustomerByID(custID);
+                var conlist= GetConsumeList(comlist, cust, ConsumeType.NoCard,(PayType)payType);
+                ConsumeMgm conMgm = new ConsumeMgm();
+                string serNo = conMgm.ConsumeForNoCard(conlist);
+                data.Success = true;
+                data.Message = serNo + "|" + conlist.FirstOrDefault().CreatedDate.ToString("yyyy/MM/dd HH:mm:ss");
+            }
+            catch(Exception ex)
+            {
+                data.Success = false;
+                data.Message = "出现错误，请联系维护人员";
+                LogHandler.Error(ex.Message.ToString());
+                data.Error = ex.ToString();
+            }
+            return Json(data,JsonRequestBehavior.AllowGet);
+        }
+        private List<ComsumeItemViewModel> JsonSerializer(string data)
+        {
+            List<ComsumeItemViewModel> list = new List<ComsumeItemViewModel>();
+            string dataitems = data.Replace("},", "}|");
+            Regex re = new Regex("\"|\\[|\\]|{|}");
+            dataitems = re.Replace(dataitems, "");
+            string[] items = dataitems.Split('|');
+
+            foreach (var item in items)
+            {
+                string[] itemAttr = item.Split(',');
+                ComsumeItemViewModel comModel = new ComsumeItemViewModel();
+                comModel.Itemid = NullHelper.Convert<int>(itemAttr[0].Split(':')[1], 0);
+                comModel.ItemName = NullHelper.Convert<string>(itemAttr[1].Split(':')[1], "");
+                comModel.ItemConNum = NullHelper.Convert<int>(itemAttr[2].Split(':')[1], 0);
+                comModel.ItemTotalPrice = NullHelper.Convert<decimal>(itemAttr[3].Split(':')[1], 0M);
+                list.Add(comModel);
+            }
+
+
+            return list;
+        }
+        private List<ConsumeVO> GetConsumeList(List<ComsumeItemViewModel> list,CustomerVO cust, ConsumeType conType,PayType paytype)
+        {
+            List<ConsumeVO> conList = new List<ConsumeVO>();
+            DateTime date = DateTime.Now;
+            list.ForEach(item =>
+            {
+                conList.Add(new ConsumeVO
+                {
+                    ItemID = item.Itemid,
+                    ItemName = item.ItemName,
+                    ConsumeStore = Emp.StroeName,
+                    ConsumeCount = item.ItemConNum,
+                    OriginalPrice = item.ItemTotalPrice,
+                    CreatedDate = date,
+                    CreatorID = Emp.UserName,
+                    LastModifiedDate = date,
+                    LastModifierID = Emp.UserName,
+                    ConsumeDate = date,
+                    ConsumeType = (int)conType,
+                    ClubCardPackageID = "",
+                    PackageDetailID = 0,
+                    ClubCardNo = "",
+                    ClubCardID = 0,
+                    CustID = cust.CustomerID,
+                    CustName = cust.Name,
+                    OriginalStore = "",
+                    RemainCount = 0,
+                    Reserved1 = Emp.StoreId.ToString(),
+                    Reserved2=paytype.ToString()
+                });
+            });
+            return conList;
         }
 	}
 }
